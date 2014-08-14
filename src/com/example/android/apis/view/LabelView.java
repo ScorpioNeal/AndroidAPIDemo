043534cb619af551e42eeffba00c44405864675f
ScorpioNeal <budongxu@gmail.com>
2014-08-14 20:01:36 +0800

    tmp

--- src/com/example/android/apis/view/LabelView.java
+++ src/com/example/android/apis/view/LabelView.java
@@ -27,168 +27,184 @@ import android.view.View;
 
 import com.example.android.apis.R;
 
-
 /**
- * Example of how to write a custom subclass of View. LabelView
- * is used to draw simple text views. Note that it does not handle
- * styled text or right-to-left writing systems.
- *
+ * Example of how to write a custom subclass of View. LabelView is used to draw
+ * simple text views. Note that it does not handle styled text or right-to-left
+ * writing systems.
+ * 
  */
 public class LabelView extends View {
-    private Paint mTextPaint;
-    private String mText;
-    private int mAscent;
-    
-    /**
-     * Constructor.  This version is only needed if you will be instantiating
-     * the object manually (not from a layout XML file).
-     * @param context
-     */
-    public LabelView(Context context) {
-        super(context);
-        initLabelView();
-    }
+	private Paint mTextPaint;
+	private String mText;
+	private int mAscent;
 
-    /**
-     * Construct object, initializing with any attributes we understand from a
-     * layout file. These attributes are defined in
-     * SDK/assets/res/any/classes.xml.
-     * 
-     * @see android.view.View#View(android.content.Context, android.util.AttributeSet)
-     */
-    public LabelView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initLabelView();
+	/**
+	 * Constructor. This version is only needed if you will be instantiating the
+	 * object manually (not from a layout XML file).
+	 * 
+	 * @param context
+	 */
+	public LabelView(Context context) {
+		super(context);
+		initLabelView();
+	}
 
-        TypedArray a = context.obtainStyledAttributes(attrs,
-                R.styleable.LabelView);
+	/**
+	 * Construct object, initializing with any attributes we understand from a
+	 * layout file. These attributes are defined in
+	 * SDK/assets/res/any/classes.xml.
+	 * 
+	 * @see android.view.View#View(android.content.Context,
+	 *      android.util.AttributeSet)
+	 */
+	public LabelView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initLabelView();
 
-        CharSequence s = a.getString(R.styleable.LabelView_text);
-        if (s != null) {
-            setText(s.toString());
-        }
+		TypedArray a = context.obtainStyledAttributes(attrs,
+				R.styleable.LabelView);
 
-        // Retrieve the color(s) to be used for this view and apply them.
-        // Note, if you only care about supporting a single color, that you
-        // can instead call a.getColor() and pass that to setTextColor().
-        setTextColor(a.getColor(R.styleable.LabelView_textColor, 0xFF000000));
+		CharSequence s = a.getString(R.styleable.LabelView_text);
+		if (s != null) {
+			setText(s.toString());
+		}
 
-        int textSize = a.getDimensionPixelOffset(R.styleable.LabelView_textSize, 0);
-        if (textSize > 0) {
-            setTextSize(textSize);
-        }
+		// Retrieve the color(s) to be used for this view and apply them.
+		// Note, if you only care about supporting a single color, that you
+		// can instead call a.getColor() and pass that to setTextColor().
+		setTextColor(a.getColor(R.styleable.LabelView_textColor, 0xFF000000));
 
-        a.recycle();
-    }
+		int textSize = a.getDimensionPixelOffset(
+				R.styleable.LabelView_textSize, 0);
+		if (textSize > 0) {
+			setTextSize(textSize);
+		}
 
-    private final void initLabelView() {
-        mTextPaint = new Paint();
-        mTextPaint.setAntiAlias(true);
-        // Must manually scale the desired text size to match screen density
-        mTextPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
-        mTextPaint.setColor(0xFF000000);
-        setPadding(3, 3, 3, 3);
-    }
+		a.recycle();
+	}
 
-    /**
-     * Sets the text to display in this label
-     * @param text The text to display. This will be drawn as one line.
-     */
-    public void setText(String text) {
-        mText = text;
-        requestLayout();
-        invalidate();
-    }
+	private final void initLabelView() {
+		mTextPaint = new Paint();
+		mTextPaint.setAntiAlias(true);
+		// Must manually scale the desired text size to match screen density
+		mTextPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
+		mTextPaint.setColor(0xFF000000);
+		setPadding(3, 3, 3, 3);
+	}
 
-    /**
-     * Sets the text size for this label
-     * @param size Font size
-     */
-    public void setTextSize(int size) {
-        // This text size has been pre-scaled by the getDimensionPixelOffset method
-        mTextPaint.setTextSize(size);
-        requestLayout();
-        invalidate();
-    }
+	/**
+	 * Sets the text to display in this label
+	 * 
+	 * @param text
+	 *            The text to display. This will be drawn as one line.
+	 */
+	public void setText(String text) {
+		mText = text;
+		requestLayout();
+		invalidate();
+	}
 
-    /**
-     * Sets the text color for this label.
-     * @param color ARGB value for the text
-     */
-    public void setTextColor(int color) {
-        mTextPaint.setColor(color);
-        invalidate();
-    }
+	/**
+	 * Sets the text size for this label
+	 * 
+	 * @param size
+	 *            Font size
+	 */
+	public void setTextSize(int size) {
+		// This text size has been pre-scaled by the getDimensionPixelOffset
+		// method
+		mTextPaint.setTextSize(size);
+		requestLayout();
+		invalidate();
+	}
 
-    /**
-     * @see android.view.View#measure(int, int)
-     */
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(measureWidth(widthMeasureSpec),
-                measureHeight(heightMeasureSpec));
-    }
+	/**
+	 * Sets the text color for this label.
+	 * 
+	 * @param color
+	 *            ARGB value for the text
+	 */
+	public void setTextColor(int color) {
+		mTextPaint.setColor(color);
+		invalidate();
+	}
 
-    /**
-     * Determines the width of this view
-     * @param measureSpec A measureSpec packed into an int
-     * @return The width of the view, honoring constraints from measureSpec
-     */
-    private int measureWidth(int measureSpec) {
-        int result = 0;
-        int specMode = MeasureSpec.getMode(measureSpec);
-        int specSize = MeasureSpec.getSize(measureSpec);
+	/**
+	 * @see android.view.View#measure(int, int)
+	 */
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		setMeasuredDimension(measureWidth(widthMeasureSpec),
+				measureHeight(heightMeasureSpec));
+	}
 
-        if (specMode == MeasureSpec.EXACTLY) {
-            // We were told how big to be
-            result = specSize;
-        } else {
-            // Measure the text
-            result = (int) mTextPaint.measureText(mText) + getPaddingLeft()
-                    + getPaddingRight();
-            if (specMode == MeasureSpec.AT_MOST) {
-                // Respect AT_MOST value if that was what is called for by measureSpec
-                result = Math.min(result, specSize);
-            }
-        }
+	/**
+	 * Determines the width of this view
+	 * 
+	 * @param measureSpec
+	 *            A measureSpec packed into an int
+	 * @return The width of the view, honoring constraints from measureSpec
+	 */
+	private int measureWidth(int measureSpec) {
+		int result = 0;
+		int specMode = MeasureSpec.getMode(measureSpec);
+		int specSize = MeasureSpec.getSize(measureSpec);
 
-        return result;
-    }
+		if (specMode == MeasureSpec.EXACTLY) {
+			// We were told how big to be
+			result = specSize;
+		} else {
+			// Measure the text
+			result = (int) mTextPaint.measureText(mText) + getPaddingLeft()
+					+ getPaddingRight();
+			if (specMode == MeasureSpec.AT_MOST) {
+				// Respect AT_MOST value if that was what is called for by
+				// measureSpec
+				result = Math.min(result, specSize);
+			}
+		}
 
-    /**
-     * Determines the height of this view
-     * @param measureSpec A measureSpec packed into an int
-     * @return The height of the view, honoring constraints from measureSpec
-     */
-    private int measureHeight(int measureSpec) {
-        int result = 0;
-        int specMode = MeasureSpec.getMode(measureSpec);
-        int specSize = MeasureSpec.getSize(measureSpec);
+		return result;
+	}
 
-        mAscent = (int) mTextPaint.ascent();
-        if (specMode == MeasureSpec.EXACTLY) {
-            // We were told how big to be
-            result = specSize;
-        } else {
-            // Measure the text (beware: ascent is a negative number)
-            result = (int) (-mAscent + mTextPaint.descent()) + getPaddingTop()
-                    + getPaddingBottom();
-            if (specMode == MeasureSpec.AT_MOST) {
-                // Respect AT_MOST value if that was what is called for by measureSpec
-                result = Math.min(result, specSize);
-            }
-        }
-        return result;
-    }
+	/**
+	 * Determines the height of this view
+	 * 
+	 * @param measureSpec
+	 *            A measureSpec packed into an int
+	 * @return The height of the view, honoring constraints from measureSpec
+	 */
+	private int measureHeight(int measureSpec) {
+		int result = 0;
+		int specMode = MeasureSpec.getMode(measureSpec);
+		int specSize = MeasureSpec.getSize(measureSpec);
 
-    /**
-     * Render the text
-     * 
-     * @see android.view.View#onDraw(android.graphics.Canvas)
-     */
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawText(mText, getPaddingLeft(), getPaddingTop() - mAscent, mTextPaint);
-    }
+		mAscent = (int) mTextPaint.ascent();
+		if (specMode == MeasureSpec.EXACTLY) {
+			// We were told how big to be
+			result = specSize;
+		} else {
+			// Measure the text (beware: ascent is a negative number)
+			result = (int) (-mAscent + mTextPaint.descent()) + getPaddingTop()
+					+ getPaddingBottom();
+			if (specMode == MeasureSpec.AT_MOST) {
+				// Respect AT_MOST value if that was what is called for by
+				// measureSpec
+				result = Math.min(result, specSize);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Render the text
+	 * 
+	 * @see android.view.View#onDraw(android.graphics.Canvas)
+	 */
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		canvas.drawText(mText, getPaddingLeft(), getPaddingTop() - mAscent,
+				mTextPaint);
+	}
 }
